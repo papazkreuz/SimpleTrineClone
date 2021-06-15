d83b98c3e193deb6e7366bbb1da06ed5424183c2
papazkreuz <85841895+papazkreuz@users.noreply.github.com>
2021-06-15 23:23:36 +0300

    Minor fixes

--- Scripts/GameData.cs
+++ Scripts/GameData.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-public static class GameData
+﻿public static class GameData
 {
     public static CharacterClass pickedClass = CharacterClass.Mage;
 }
